3b195f6fa2f051f230fa4fe33826a8c9ce3df517
Adam Keith <adamchristopherkeith@gmail.com>
2017-03-22 16:47:59 -0400

    Pretty much added the whole gui. Now need to make buttons inactive when they should be.

--- buttons.py
+++ buttons.py
@@ -6,23 +6,23 @@ Created on Wed Mar 22 00:29:25 2017
 """
 import tkinter as tk
 
-class PlayerButton(tk.button):
+class PlayerButton(tk.Button):
     # pass player, add species
     pass
 
-class SpeciesButton(tk.button):
+class SpeciesButton(tk.Button):
     # + pop, body size, feed, add trait, eat
     pass
 
-class PlantPhaseButton(tk.button):
+class PlantPhaseButton(tk.Button):
     # trait use    
     pass
 
-class EvolvePhaseButton(tk.button):
+class EvolvePhaseButton(tk.Button):
     #pass, add species, +pop,bodysize, add trait, trait use
     pass
 
-class FeedPhaseButton(tk.button):
+class FeedPhaseButton(tk.Button):
     # feed, eat, pass?
     pass
 
@@ -39,6 +39,16 @@ class AddPopulationButton(EvolvePhaseButton, SpeciesButton):
     pass
 
 class AddBodySizeButton(EvolvePhaseButton, SpeciesButton):
+    pass
+
+class AddSpeciesButton(EvolvePhaseButton, PlayerButton):
+    pass
+
+class AddSpeciesLeftButton(AddSpeciesButton):
+    # __init__(self,master, text="Add Species Left")
+    pass
+
+class AddSpeciesRightButton(AddSpeciesButton):
     pass
 
 class FeedButton(FeedPhaseButton, SpeciesButton):
--- components.py
+++ components.py
@@ -4,6 +4,9 @@ Created on Sun Mar 19 18:43:22 2017
 
 @author: ACKWinDesk
 
+Design:
+    Only one human player or less currently intended
+
 Possible Issues:
     All widgets have parent instance as attribute
     
@@ -14,11 +17,46 @@ import tkinter as tk
 import random
 from buttons import *
 
+class TraitCard(tk.Frame):
+    """Frame containing trait card information"""
+    # Need a database to load trait cards from
+
+    def __init__(self, hand=None, name="None", ability="None", food=0):
+        tk.Frame.__init__(self, hand)
+        self.master = hand
+        self.name = name
+        self.ability = ability
+        self.food = food   # food that can be planted
+        self.create_widgets()
+        
+    def create_widgets(self):
+        """Create Trait Card GUI elements"""
+        self.name_label = tk.Label(self, text=self.name)
+        self.ability_label = tk.Label(self, text=self.ability)
+        self.food_label = tk.Label(self, text="Food:")
+        self.use_button = TraitUseButton(self, text="USE", command=self.use)
+        
+        self.name_label.pack()  
+        self.ability_label.pack()  
+        self.food_label.pack()  
+        self.use_button.pack()  
+  
+    def change_hands(self, new_hand=None):
+        """Change master frame of this card"""
+        self.master = new_hand
+      
+    def remove_from_hand(self):
+        """Remove this frame from hand frame"""
+        pass
+                                         
+    def use(self):
+        """Signal to hand that this card was used"""
+        master.use_trait_card(self)
 
 class Evolution(tk.Frame):    
     # Generate list of all cards in game
     all_cards = []
-    all_cards.extend([Foraging() for i in range(20)])
+    all_cards.extend([TraitCard() for i in range(20)]) 
     TRAIT_LIMIT = 3
     TRAIT_LIMIT_2p = 2
     BASE_CARD_DRAW = 3
@@ -40,44 +78,15 @@ class Evolution(tk.Frame):
 
         # Deck of cards
         self.deck = Evolution.all_cards
+        for card in self.deck:
+            card.change_hands(self)
         random.shuffle(self.deck)
 
         # Discard pile        
         self.discard = []
         
-        ### BUILD GUI ####
-        self.grid()
-        # Set up information labels
-        self.cards_left = tk.StringVar()
-        self.era = tk.IntVar()
-        self.phase_name = tk.StringVar()
-        self.phase_name.set("GAME SETUP")
-        
-        self.cards_left_label = tk.Label(self, textvariable=self.cards_left)
-        self.era_label1 = tk.Label(self, text="Era: ")
-        self.era_label2 = tk.Label(self, textvariable=self.cards_left)
-        self.phase_label1 = tk.Label(self, text="Phase: ")
-        self.phase_label2 = tk.Label(self, textvariable=self.phase_name)
-        self.food_label1 = tk.Label(self, text='Food on Watering Hole: ')
-        self.food_label2 = tk.Label(self, textvariable=self.food_on_hole)
-        
-        self.cards_left_label.grid(row=0,column=0)
-        self.era_label1.grid(row=0,column=1)
-        self.era_label2.grid(row=0,column=2)
-        self.phase_label1.grid(row=0,column=3)
-        self.phase_label2.grid(row=0,column=4)
-        self.food_label1.grid(row=0,column=5)
-        self.food_label2.grid(row=0,column=6)
-                
-        # Set up players
-        self.num_players = num_players
-        #self.players = [Player(self) for i in range(self.num_players)]
-        self.players = [AI_Player(self) for i in range(self.num_players-1)]
-        self.players.append(Human_Player(self))
-#        for player in self.players:
-#            player.grid()
-
         # Set starting player        
+        self.num_players = num_players
         if first_player is None:
             first_player = random.randint(0, self.num_players-1)
         self.first_player = first_player  # current first player
@@ -85,7 +94,55 @@ class Evolution(tk.Frame):
         self.trait_limit = Evolution.TRAIT_LIMIT
         if self.num_players == 2: # special rules for 2 player
             self.trait_limit = Evolution.TRAIT_LIMIT_2p
-            # randomly remove 40 cars
+            # randomly remove 40 cars        
+            
+        self.cards_left = tk.IntVar()
+        self.cards_left.set(len(self.deck))
+        self.era = tk.IntVar()
+        self.era.set(0)
+        self.phase_name = tk.StringVar()
+        self.phase_name.set("GAME SETUP")
+        self.create_widgets()
+        
+        # Set up players' frames
+        #self.players = [Player(self) for i in range(self.num_players)]
+        self.players = [AI_Player(self) for i in range(self.num_players-1)]
+        self.players.append(Human_Player(self))
+        for player in self.players:
+            player.pack(fill='both',expand=True,anchor="nw")
+
+    def create_widgets(self):
+        ### BUILD GUI ####
+        #self.grid()
+        #self.pack(side="top", fill="both", expand=True)
+        # Set up information labels
+        self.game_information_frame = tk.Frame(self)
+        gi = self.game_information_frame
+        gi.pack(fill="x",expand=False,anchor="nw")
+        self.cards_left_label1 = tk.Label(gi, text="Cards Left in Deck:", 
+                                          fg='white', bg='black')
+        self.cards_left_label2 = tk.Label(gi, textvariable=self.cards_left,
+                                          fg='white', bg='black')
+        self.era_label1 = tk.Label(gi, text="|Era:", fg='white', bg='black')
+        self.era_label2 = tk.Label(gi, textvariable=self.era, 
+                                   fg='white', bg='black')
+        self.phase_label1 = tk.Label(gi, text="|Phase:",
+                                     fg='white', bg='black')
+        self.phase_label2 = tk.Label(gi, textvariable=self.phase_name,
+                                     fg='white', bg='black')
+        self.food_label1 = tk.Label(gi, text="|Food on Watering Hole:",
+                                    fg='white', bg='black')
+        self.food_label2 = tk.Label(gi, textvariable=self.food_on_hole,
+                                    fg='white', bg='black')
+        
+        self.cards_left_label1.pack(side="left",fill='x',expand=True,anchor="nw")
+        self.cards_left_label2.pack(side="left",fill='x',expand=True,anchor="nw")
+        self.era_label1.pack(side="left",fill='x',expand=True,anchor="nw")
+        self.era_label2.pack(side="left",fill='x',expand=True,anchor="nw")
+        self.phase_label1.pack(side="left",fill='x',expand=True,anchor="nw")
+        self.phase_label2.pack(side="left",fill='x',expand=True,anchor="nw")
+        self.food_label1.pack(side="left",fill='x',expand=True,anchor="nw")
+        self.food_label2.pack(side="left",fill='x',expand=True,anchor="nw")
 
     def gui_phase_change(self):
         """Make certain widgets inactive during different game phases"""
@@ -127,8 +184,9 @@ class Evolution(tk.Frame):
                 self.check_for_empty_deck()
                 # actually pops last element so pretend deck is flipped
                 player.hand.add_card(self.deck.pop())
+        # Don't actually set cards left until all cards have been handed out
         if self.continue_game:
-            self.cards_left.set('Cards Left in Deck: ' + len(self.deck))
+            self.cards_left.set(len(self.deck))
         else:
             self.cards_left.set(0)
 
@@ -157,39 +215,6 @@ class Evolution(tk.Frame):
         pass
         
 
-class TraitCard(tk.Frame, name="None", ability="None", food=0):
-    """Frame containing trait card information"""
-    # Need a database to load trait cards from
-
-    def __init__(self, hand):
-        tk.Frame.__init__(self, hand)
-        self.master = hand
-        self.name = name
-        self.ability = ability
-        self.food = food   # food that can be planted
-        self.create_widgets()
-        
-    def create_widgets(self):
-        """Create Trait Card GUI elements"""
-        self.name_label = tk.Label(self, text=self.name)
-        self.ability_label = tk.Label(self, text=self.ability)
-        self.food_label = tk.Label(self, text="Food Eaten: ")
-        self.use_button = TraitUseButton(self, text="USE", command=self.use)
-        
-        self.name_label.grid()  
-        self.ability_label.grid()  
-        self.food_label.grid()  
-        self.use_button.grid()  
-        
-    def remove_from_hand(self):
-        """Remove this frame from hand frame"""
-        pass
-                                         
-    def use(self):
-        """Signal to hand that this card was used"""
-        master.use_trait_card(self)
-
-
 class Hand(tk.Frame):
     """Frame containing players' hands. 
     For humans, display frame below player's species"""
@@ -199,15 +224,24 @@ class Hand(tk.Frame):
         self.empty = tk.BooleanVar()
         self.empty.set(True)
         self.empty.trace('w', self.toggle_empty_hand)
-        self.num_cards = 0
+        self.num_cards = tk.IntVar()
+        self.num_cards.set(0)
         self.card_list = []
+
+    def create_widgets(self):
+        """Pack TraitCards contained in this hand"""
+        # Refresh packing by removing all packed items?
+        for card in self.card_list:
+            card.pack(side="left")
 
     def add_card(self, card):
         """Add card to hand"""
+        card.change_hands(self)
         self.card_list.append(card)
+        self.num_cards.set(self.num_cards.get()+1)
         if self.empty.get() is True:
-            self.empty.set(False)    
-            
+            self.empty.set(False)
+
     def activate_hand(self):
         pass
 
@@ -215,15 +249,15 @@ class Hand(tk.Frame):
         for widget in self.winfo_children():
             widget.config(state='disabled')
     
-    def toggle_empty_hand(self):
+    def toggle_empty_hand(self, *args):
         """Disable buttons if hand is empty"""
         # (depending on phase i.e. intelligence
         pass
-
+    
     def use_trait_card(self, card):
         """Use trait card from hand for a variety of things"""
         # remove card from the list
-        self.num_cards -= 1        
+        self.num_cards.set(self.num_cards.get()-1)  
         temp = self.card_list.pop(self.card_list.index(card))
         if self.num_cards == 0:
            self.empty.set(True) 
@@ -241,6 +275,16 @@ class Hand(tk.Frame):
 class Player(tk.Frame):
     """Defines base player. Mechanics of playing. Child classes define special
     humand and AI GUI elements"""
+    def change_player_state(self):
+        """Change player button states depending on if active player"""
+        if self.active_player.get() is True:
+            # Get game phase and unlock respective buttons?
+            # or should game do that
+            pass
+        else:
+            #shutdown buttons?
+            pass
+        
     def __init__(self, game):
         tk.Frame.__init__(self,game)
         self.master = game
@@ -248,50 +292,82 @@ class Player(tk.Frame):
         self.active_player.set(False)
         self.active_player.trace('w', self.change_player_state)
         self.num_species = 1
-        self.food_eaten = 0
-        
-        # need label for number of cards in hand
-        
-        # food stored
-        # pass button
-        
-        #self.label = tk.Label(self, text='Number of ')
-        #self.label.grid(row=2,column=0)
-        #self.label.grid()
-        #self.grid(columnspan=1)
-        self.grid(sticky='W')
-        #self.pack(side='top')
-        self.pop_label = tk.Label(self, text="Population: ")
-        self.bs_label = tk.Label(self, text="Body Size: ")
-        #self.pop_label.pack()
-        #self.bs_label.pack()
-        self.pop_label.grid(row=0,column=0, rowspan=2, columnspan=1, sticky='NW')
-        self.bs_label.grid(row=1,column=0, rowspan=2, columnspan=1, sticky='NW')
-        
+        self.food_eaten = tk.IntVar()        
+        self.food_eaten.set(0)
+
+        self.ecosystem = []
+        self.add_species_right()
+
+        self.create_player_info_frame()
+        self.pack_species_and_hand()
         # Setup Ecosystem (a list of species)
-        self.ecosystem = [Species(self)]
-        
-        #Place species frames
-        self.ecosystem[0].grid(row=0, column=1, sticky='E',rowspan=2)
 
         # Add "add species" buttons left and right        
         
         ## add label for population and bodysize for all species
         # Active player border highlight
         
-        def update_species_frames(self):
-            """Update species frames if some have gone extinct?"""
-            pass
+    def create_player_info_frame(self):
+        """Create Species GUI elements"""
+        self.hand = Hand(self)
+        self.hand.add_card(TraitCard(self.hand))
+        self.hand.add_card(TraitCard(self.hand))
+        self.hand.create_widgets()
         
-        def change_player_state(self):
-            """Change player button states depending on if active player"""
-            if self.active_player.get() is True:
-                # Get game phase and unlock respective buttons?
-                # or should game do that
-                pass
-            else:
-                #shutdown buttons?
-                pass
+        self.player_info_frame = tk.Frame(self)
+        pif = self.player_info_frame
+        self.cards_in_hand_label1 = tk.Label(pif, text="# of Cards: ")
+        self.cards_in_hand_label2 = tk.Label(pif, textvariable=self.hand.num_cards)
+        self.food_label1 = tk.Label(pif, text="Food in Bag:")
+        self.food_label2 = tk.Label(pif, textvariable=self.food_eaten)
+        self.pass_button = PassButton(pif,text="PASS", command=None)
+        
+        self.player_info_frame.pack(fill='x',expand=False,anchor="ne")
+        self.cards_in_hand_label1.pack(side='left',fill='x',expand=True,anchor="nw")
+        self.cards_in_hand_label2.pack(side='left',fill='x',expand=True,anchor="nw")
+        self.food_label1.pack(side='left',fill='x',expand=True,anchor="nw")
+        self.food_label2.pack(side='left',fill='x',expand=True,anchor="nw")
+        self.pass_button.pack(side='left',fill='x',expand=True,anchor="nw")
+    
+    def pack_species_and_hand(self):
+        # Pack hand
+        self.hand.pack(side="bottom")      
+        self.pack_species()
+        
+    def pack_species(self):
+        #Pack species frames and add species buttons
+        self.add_species_left_button = AddSpeciesLeftButton(
+                self, text="Add Species Left", command=self.add_species_left)
+        self.add_species_right_button = AddSpeciesRightButton(
+                self, text="Add Species Right", command=self.add_species_right)
+        self.add_species_left_button.pack(side="left")
+        for species in self.ecosystem:
+            species.pack(side="left",expand=True)
+        self.add_species_right_button.pack(side="right")
+        
+    
+
+        
+    def add_food_to_bag(self):
+        """Add food from all species to food bag"""
+        self.food_eaten.set(sum([species.food.get() for species in self.ecosystem]))
+
+    def add_species_left(self):
+        """Add one species to left of the current ecosystem"""        
+        self.ecosystem.insert(0,Species(self))
+
+    def add_species_right(self):
+        """Add one species to right of the current ecosystem"""        
+        self.ecosystem.append(Species(self))
+
+
+        
+    
+    def update_species_frames(self):
+        """Update species frames if some have gone extinct?"""
+        pass
+    
+
             
 #    # Actions    
 #    # Discard to add new species to ecosystem
@@ -321,6 +397,11 @@ class AI_Player(Player):
         Player.__init__(self, game)
         self.config(background='blue')
 
+    def pack_species_and_hand(self):
+        # Don't Pack hand because humans shouldn't see that
+        #self.hand.pack(side="bottom")      
+        self.pack_species()
+        
     def take_turn():
         pass
     def feed():
@@ -365,23 +446,25 @@ class Species(tk.Frame):
         self.eat_button = EatButton(self, text="EAT ME")
         self.add_trait_buttons = []
         self.add_trait_labels = []
-        for t in range(master.master.trait_limit):
+        for t in range(self.master.master.trait_limit):
             self.add_trait_buttons.append(AddTraitButton(self,text="Add Trait"))
             self.add_trait_labels.append(tk.Label(self, text="No Trait"))
         self.population_label1 = tk.Label(self, text="Population: ")
         self.population_label2 = tk.Label(self, textvariable=self.population)
         self.body_size1 = tk.Label(self, text="Body Size: ")
         self.body_size2 = tk.Label(self, textvariable=self.body_size)
-        self.add_population_button = AddPopulationButton(self, text="+1 Population",
+        self.add_population_button = AddPopulationButton(self,
+                                         text="+1 Population",
                                          command=self.add_population)
-        self.add_body_size_button = AddBodySizeButton(self, text="+1 Body Size",
-                                         command=self.add_body_size)  
+        self.add_body_size_button = AddBodySizeButton(self, 
+                                        text="+1 Body Size",
+                                        command=self.add_body_size)  
                                          
         self.feed_button.grid(row=0,column=0)           
         self.food_label1.grid(row=0,column=1)   
         self.food_label2.grid(row=0,column=2)   
         self.eat_button.grid(row=0,column=3)         
-        for t in range(master.master.trait_limit):            
+        for t in range(self.master.master.trait_limit):            
             self.add_trait_buttons[t].grid(row=t+1,column=0)
             self.add_trait_labels[t].grid(row=t+1,column=1,columnspan=3)       
                                          
@@ -398,7 +481,7 @@ class Species(tk.Frame):
         # change self.hungry
         pass
     
-    def set_pop_dependent_flags(self):
+    def set_population_dependent_flags(self):
         """Set flags depending on how much pop there is"""
         # check if self.food <= self.population
         # change self.hungry
@@ -479,6 +562,7 @@ class TraitRepeatException(Exception):
 
 
 game = Evolution()
+game.pack(fill="both", expand=True)
 game.mainloop()
 #
 #def main(): #run mainloop 
